850204bc64c1394dd92c70321eb7e612fccf6f39
ifhuni <huni1480@naver.com>
2025-01-24 11:25:30 +0900

    build: spring 프로젝트

---
+++ src/test/java/com/climbers/hangangactivity/HangangactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climbers.hangangactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangangactivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
