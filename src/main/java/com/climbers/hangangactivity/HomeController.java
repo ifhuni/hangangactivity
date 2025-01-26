ddd5adb9ef4c9dcd5bfff10af26b3b3102b06b82
ifhuni <huni1480@naver.com>
2025-01-26 23:48:35 +0900

    feat: 프폰트 헤더 작성

--- src/main/java/com/climbers/hangangactivity/HomeController.java
+++ src/main/java/com/climbers/hangangactivity/HomeController.java
@@ -1,13 +1,15 @@
 package com.climbers.hangangactivity;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
-    public String index() {
-        return "index"; // templates/index.html 파일을 찾음
+    public String index(Model model) {
+        model.addAttribute("message", "Welcome to Thymeleaf!");
+        return "index";
     }
+
 }
