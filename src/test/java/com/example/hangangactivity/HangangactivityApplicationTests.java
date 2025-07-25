54dda8e7829344c79ff9d12928b6df44181f05c6
ifhuni <huni1480@gmail.com>
2025-07-25 09:17:09 +0900

    Spring Boot 프로젝트 생성

---
+++ src/test/java/com/example/hangangactivity/HangangactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangangactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangangactivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
